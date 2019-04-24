fcb3235f60a9ffcaaadf2322ea76c393282a748e
adityasundaram <aditya1sundaram@gmail.com>
2019-04-24 10:48:04 -0700

    updating scripts

--- scripts/compute_neighbors.py
+++ scripts/compute_neighbors.py
@@ -2,45 +2,63 @@ import gzip, json, os , sys
 import re
 from collections import defaultdict
 linecount = -1
-mapOfNeighbors = defaultdict(list)
+mapOfNeighbors = defaultdict(dict)
 
-# Add all the languages that we want to extract here
-languages = ['en']
-glossary = set()
-def clean(string: str):
-    string = ' '.join(string.split()).strip()
-    return string
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
-with gzip.GzipFile('wikidata.gz', 'r') as fin:
+
+with gzip.GzipFile('../wikidata.gz', 'r') as fin:
     for line in fin:
         linecount+=1
         if linecount == 0 or len(line) < 5:
             continue
 
         if linecount % 10000 == 0:
-            print(linecount)
+            printProgressBar(linecount, 52000000, prefix = 'Progress:', suffix = 'Complete', length = 50)
         js = line.strip().decode('utf-8')[:-1]
         try:
             data = json.loads(js)
             # Extract labels based on languages set initially
             doc_id = data['id']
             statements = data['claims']
+            statement_map = defaultdict(list)
             for statement in statements:
                 val = statements[statement]
                 assert isinstance(val, list), "This should be list"
                 for details in val:
-                    #print(details)
                     if details['type'] == "statement" and 'mainsnak' in details.keys():
                         if details['mainsnak'] != {} and details['mainsnak']['datatype'] == 'wikibase-item' and details['mainsnak']['snaktype']=='value':
                             try:
                                 id = details['mainsnak']['datavalue']['value']['id']
-                                mapOfNeighbors[id].append(doc_id)
+                                #mapOfNeighbors[id].append(doc_id)
+                                statement_map[statement].append(id)
                             except Exception as e:
                                 print("Exception while processing, but continuing {}".format(e.args))
                                 pass
+            mapOfNeighbors[doc_id] = statement_map
         except Exception as e:
             print("Error while loading a json line {}".format(e.args))
-with open("neighbor_map.json","w") as out:
+        
+with open("neighbor_map_with_properties.json","w") as out:
     out.write(json.dumps(mapOfNeighbors))
 
 print("Done")
--- scripts/edge_prune.py
+++ scripts/edge_prune.py
@@ -12,24 +12,77 @@ with open(args.dictionary, 'r') as fin:
     line = fin.readline()
     data = json.loads(line)
 print("Neighbor map loaded")
-
 print("Number of keys {}".format(len(data)))
-block = set()
-edge_list = open('edge_list_temp.csv', 'w')
-with open(args.blacklist, 'r') as fin:
-    line = fin.readline()
-    block = set(json.loads(line.strip()))
+allow = set(data.keys())
+out_file = open('edge_list_film.csv','w')
+for key in allow:
+    if key not in data:
+        continue
+    sts = data[key]
+    for st in sts:
+        edges = sts[st]
+        for e in edges:
+            out_file.write("{}\t{}\n".format(key,e))
+
+"""
+allow = set()
+for filename in os.listdir(args.blacklist):
+    with open(args.blacklist+'/'+filename) as fin:
+        for line in fin:
+            x = line.strip().split('/')
+            if len(x) > 1:
+                allow.add(x[-1])
+
+print("whitelist has {} entities for film graph".format(len(allow)))
+final_graph = dict()
+for key in allow:
+    if key not in data:
+        continue
+    sts = data[key]
+    final = dict()
+    for st in sts:
+        edges = sts[st]
+        # filter edges here
+        edges = [x for x in edges if x in allow]
+        if edges:
+            final[st] = edges
+    final_graph[key] = final
+
+with open('film_graph.json','w') as fout:
+    fout.write(json.dumps(final_graph))
+print("Done")
+"""
+"""
+edge_list = open('train2id.txt', 'w')
+nodes = open('entity2id.txt','w')
+props = open('relation2id.txt','w')
+allow = set(data.keys())
+qid = 0
+pid = 0
+q = dict()
+p = dict()
 print("computed blacklist")
-print("Size of blacklist {}".format(len(block)))
-for key in data:
-    if 'fw' in data[key]:
-        ids = list(data[key]['fw'])
-        for id in ids:
-            if key not in block and id not in block:
-                edge_list.write("{},{}\n".format(key[1:],id[1:]))
-    if 'bk' in data[key]:
-        ids = list(data[key]['bk'])
-        for id in ids:
-            if key not in block and id not in block:
-                edge_list.write("{},{}\n".format(id[1:],key[1:]))
+print("Size of blacklist {}".format(len(allow)))
+#props = set({'P31','P279'})
+for key in allow:
+    nkey = str(key)
+    sts = data[nkey]
+    if nkey not in q:
+        q[nkey] = qid
+        qid+=1
+        nodes.write("{}\t{}\n".format(nkey, q[nkey]))
+    for st in sts:
+        edge_ends = sts[st]
+        if st not in p:
+            p[st] = pid
+            pid+=1
+            props.write("{}\t{}\n".format(st,p[st]))
+        for edge in edge_ends:
+            if edge not in q:
+                q[edge] = qid
+                qid+=1
+                nodes.write("{}\t{}\n".format(edge,q[edge]))
+            edge_list.write("{}\t{}\t{}\n".format(q[nkey],q[edge],p[st])) 
+        #exit(0)
+"""
 print("Completed")
